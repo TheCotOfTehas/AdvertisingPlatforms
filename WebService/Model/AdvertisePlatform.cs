529703775adcfca02b32027bbb666174ae13cc95
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2025-07-29 21:36:08 +0300

    Add model AdvertisePlatform

---
+++ WebService/Model/AdvertisePlatform.cs
@@ -0,0 +1,14 @@
+﻿namespace WebService.Model
+{
+    public class AdvertisePlatform
+    {
+        public string Name { get; set; }
+        public List<string> Locations { get; set; }
+
+        public AdvertisePlatform(string name, List<string> locations)
+        {
+            Name = name;
+            Locations = locations;
+        }
+    }
+}
