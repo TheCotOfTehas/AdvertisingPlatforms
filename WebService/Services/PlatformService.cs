d63daf841f00634d4cf1b93d283a2e595ee0af03
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2025-07-29 21:53:58 +0300

    AddServiceWithoutImplementation

---
+++ WebService/Services/PlatformService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Concurrent;
+using WebService.Model;
+
+namespace WebService.Services
+{
+    public class PlatformService
+    {
+        private readonly ConcurrentDictionary<string, List<AdvertisePlatform>> advertisePlatform;
+
+        public void LoadAFromFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetLocation(string location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
